a10cfaf8450a4af48fff6f5edfdd30faedaad5b1
Sebasthian Ramirez <72695645@continental.edu.pe>
2024-12-03 10:52:00 -0500

    Creación de modelos, migraciones y controladores de Vaso de Leche

---
+++ app/Http/Controllers/vaso-de-leche-controllers/VasoLecheFamilyMemberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VasoLecheFamilyMember;
+use Illuminate\Http\Request;
+
+class VasoLecheFamilyMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VasoLecheFamilyMember $vasoLecheFamilyMember)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VasoLecheFamilyMember $vasoLecheFamilyMember)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VasoLecheFamilyMember $vasoLecheFamilyMember)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VasoLecheFamilyMember $vasoLecheFamilyMember)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/vaso-de-leche-controllers/VasoLecheFamilyMemberProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VasoLecheFamilyMemberProduct;
+use Illuminate\Http\Request;
+
+class VasoLecheFamilyMemberProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VasoLecheFamilyMemberProduct $vasoLecheFamilyMemberProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VasoLecheFamilyMemberProduct $vasoLecheFamilyMemberProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VasoLecheFamilyMemberProduct $vasoLecheFamilyMemberProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VasoLecheFamilyMemberProduct $vasoLecheFamilyMemberProduct)
+    {
+        //
+    }
+}
